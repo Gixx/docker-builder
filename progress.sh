acd557c3ed22d4604ea6310dd4ac67b244bf4928
Gabor Ivan <gabor.ivan@westwing.de>
2016-12-23 11:30:34 +0100

    Change progress to avoid too many dots.

--- progress.sh
+++ progress.sh
@@ -2,9 +2,19 @@
 
 "$@" &
 
-echo ""
+SAVE_CURSOR=$(tput sc)
+RESTORE_CURSOR=$(tput rc)
+CLEAR=$(tput el)
+COUNTER=1
+
+echo $SAVE_CURSOR > /dev/tty
 
 while kill -0 $! &> /dev/null; do
-    printf '.' > /dev/tty
+    if [[ $COUNTER > 5 ]]; then
+        COUNTER=1
+        echo -n $RESTORE_CURSOR$CLEAR > /dev/tty
+    fi
+    echo -n '.' > /dev/tty
+    let ++COUNTER
     sleep 1
 done
