46aa9c31430082ceceae0ff0847765de82b8866f
Gabor Ivan <gabor.ivan@westwing.de>
2016-12-23 09:22:54 +0100

    Add some eyecandy

--- install.sh
+++ install.sh
@@ -1,22 +1,56 @@
 #!/bin/bash
 dir="$(dirname "$0")"
+TERM=xterm
+tput init
+tput clear
 cd $dir
 
 which docker-machine &>/dev/null
 
+SAVE_CURSOR=$(tput sc)
+RESTORE_CURSOR=$(tput rc)
+
 BOLD=$(tput bold)
 UNDERLINE=$(tput smul)
+
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 YELLOW=$(tput bold;tput setaf 3)
 MAGENTA=$(tput bold;tput setaf 5)
+
 ERROR=$(tput bold;tput setb 4)
 WARNING=$(tput bold;tput setaf 0;tput setb 6)
 NORMAL=$(tput sgr0)
 
+
+
 function finish() {
     echo ""
     echo "Finish"
+}
+
+function printWarning() {
+    col=`expr $(tput cols) - ${#1} - 12`
+    echo $WARNING
+    echo -n "   WARNING: "
+    echo -n $1
+    printf "%*s" $col
+    echo $NORMAL
+}
+
+function printError() {
+    col=`expr $(tput cols) - ${#1} - 10`
+    echo $ERROR
+    echo -n "   ERROR: "
+    echo -n $1
+    printf "%*s" $col
+    echo $NORMAL
+}
+
+function printHeadline() {
+    echo $UNDERLINE$GREEN;
+    echo $1
+    echo -n $NORMAL
 }
 
 VM_NAME="myproject"
@@ -39,23 +73,30 @@ function run() {
 # DEV
 cp ./install.sh /tmp/xxx
 # DEV END
-    echo $UNDERLINE$GREEN;
-    echo -n "Welcome to the Docker-Builder.$NORMAL"
+    echo $GREEN;
+    echo "  ██████╗  ██████╗  ██████╗██╗  ██╗███████╗██████╗       ██████╗ ██╗   ██╗██╗██╗     ██████╗ ███████╗██████╗"
+    echo "  ██╔══██╗██╔═══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗      ██╔══██╗██║   ██║██║██║     ██╔══██╗██╔════╝██╔══██╗"
+    echo "  ██║  ██║██║   ██║██║     █████╔╝ █████╗  ██████╔╝█████╗██████╔╝██║   ██║██║██║     ██║  ██║█████╗  ██████╔╝"
+    echo "  ██║  ██║██║   ██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗╚════╝██╔══██╗██║   ██║██║██║     ██║  ██║██╔══╝  ██╔══██╗"
+    echo "  ██████╔╝╚██████╔╝╚██████╗██║  ██╗███████╗██║  ██║      ██████╔╝╚██████╔╝██║███████╗██████╔╝███████╗██║  ██║"
+    echo "  ╚═════╝  ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝      ╚═════╝  ╚═════╝ ╚═╝╚══════╝╚═════╝ ╚══════╝╚═╝  ╚═╝"
+    echo $NORMAL
+
+    echo "Welcome to the Docker-Builder."
     echo "";
     echo "This script will create a PHP7 Docker VM for your GitHub project."
 
     # Check docker
     if [ $? -ne 0 ]; then
-        echo $ERROR
-        echo -n "   ERROR: Please install docker toolbox. https://www.docker.com/products/docker-toolbox $NORMAL"
+        printError "Please install docker toolbox. https://www.docker.com/products/docker-toolbox"
         exit 1
     fi
 
     # Check install directory
-    if [ "$(ls -A $dir)" ]; then
-        echo $WARNING
-        echo -n "   WARNING: current directory ($PWD) is not empty!$NORMAL"
-        echo ""
+
+    if [ "$(find . ! -name install.sh ! -name .)" ]; then
+        printWarning "Current directory ($PWD) is not empty!"
+
         echo "$BOLD Do you want to delete all files?$NORMAL"
         echo "  * Note: changes cannot be undone!"
         read -p "    [y/n]> " answer;
@@ -84,69 +125,65 @@ cp -f /tmp/xxx ./install.sh
     fi
 
     # Clone user's project
-    echo "";
-    echo "Type the name of your GitHub project (in format:  <Namespace>/<Project>), followed by [ENTER]:"
-    read -p "> " project
-    echo "";
+    printHeadline "Type the name of your GitHub project (in format:  <Namespace>/<Project>), followed by [ENTER]:"
+    read -p "  > $UNDERLINE$SAVE_CURSOR                         $RESTORE_CURSOR" project
+    echo $NORMAL;
     echo -n "Clone '$project' into the ./sources folder "
     ./progress.sh git clone git@github.com:$project.git ./sources &> /dev/null
 
     if [[ ! -d "./sources" ]]; then
-        echo $ERROR
-        echo -n "   ERROR: The $project could not be cloned. $NORMAL"
+        printError "The $project could not be cloned!"
         exit 1
     else
         echo " Done"
     fi
 
     # Collect informations
-    echo $UNDERLINE$GREEN
-    echo "In the following, we need you to give some setup data.$NORMAL"
+    printHeadline "In the following, we need you to give some setup data."
 
-    read -p "  * VM name (leave blank for default: $VM_NAME)> " input;
+    read -p "$NORMAL  * VM name (default: $VM_NAME)                                        > $UNDERLINE$SAVE_CURSOR                         $RESTORE_CURSOR" input;
     if [[ ! -z "$input" ]]; then
         VM_NAME=$input
     fi
 
-    read -p "  * VM timezone (leave blank for default: $VM_TIMEZONE)> " input;
+    read -p "$NORMAL  * VM timezone (default: $VM_TIMEZONE)                                > $UNDERLINE$SAVE_CURSOR                         $RESTORE_CURSOR" input;
     if [[ ! -z "$input" ]]; then
         VM_TIMEZONE=$input
     fi
 
-    read -p "  * VM domain name (leave blank for default: $VM_DOMAIN)> " input;
+    read -p "$NORMAL  * VM domain name (default: $VM_DOMAIN)                             > $UNDERLINE$SAVE_CURSOR                         $RESTORE_CURSOR" input;
     if [[ ! -z "$input" ]]; then
         VM_DOMAIN=$input
     fi
 
-    read -p "  * VM document root: relative to the ./source folder (leave blank for default: $VM_DOCROOT)> " input;
+    read -p "$NORMAL  * VM document root: relative to the ./source folder (default: $VM_DOCROOT) > $UNDERLINE$SAVE_CURSOR                         $RESTORE_CURSOR" input;
     if [[ ! -z "$input" ]]; then
         VM_DOCROOT=$input
     fi
 
-    echo $UNDERLINE$GREEN
-    echo "Please enter the MySQL credentials for the VM.$NORMAL"
+    printHeadline "Please enter the MySQL credentials for the VM."
 
-    read -p "  * MySQL user (leave blank for default: $VM_MYSQL_USER)> " input;
+    read -p "$NORMAL  * MySQL user (default: $VM_MYSQL_USER)                                          > $UNDERLINE$SAVE_CURSOR                         $RESTORE_CURSOR" input;
     if [[ ! -z "$input" ]]; then
         VM_MYSQL_USER=$input
     fi
 
-    read -p "  * MySQL password (leave blank for default: $VM_MYSQL_PASSWORD)> " input;
+    read -p "$NORMAL  * MySQL password (default: $VM_MYSQL_PASSWORD)                                  > $UNDERLINE$SAVE_CURSOR                         $RESTORE_CURSOR" input;
     if [[ ! -z "$input" ]]; then
         VM_MYSQL_PASSWORD=$input
     fi
 
-    read -p "  * MySQL database (leave blank for default: $VM_MYSQL_DATABASE)> " input;
+    read -p "$NORMAL  * MySQL database (default: $VM_MYSQL_DATABASE)                                 > $UNDERLINE$SAVE_CURSOR                         $RESTORE_CURSOR" input;
     if [[ ! -z "$input" ]]; then
         VM_MYSQL_DATABASE=$input
     fi
 
-    read -p "  * MySQL root password (leave blank for default: $VM_MYSQL_ROOTPASSWORD)> " input;
+    read -p "$NORMAL  * MySQL root password (default: $VM_MYSQL_ROOTPASSWORD)                             > $UNDERLINE$SAVE_CURSOR                         $RESTORE_CURSOR" input;
     if [[ ! -z "$input" ]]; then
         VM_MYSQL_ROOTPASSWORD=$input
     fi
 
-    echo ""
+    echo $NORMAL
     echo "Data saved, patching Docker resources. "
 }
 run
